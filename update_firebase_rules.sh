5454b3473c89c2d3cc0328cc9ae132bd6e80f1d2
heng meng <heng.meng@mobivity.com>
2017-06-30 16:58:36 -0700

    removes some fields from user table

--- update_firebase_rules.sh
+++ update_firebase_rules.sh
@@ -3,4 +3,4 @@ echo 'Current rules:'
 curl 'https://raccoon-c86bb.firebaseio.com/.settings/rules.json?auth=vTmi70GaTA2uIqbZ2bFWvpJvMGqoJvdhdcQMUxSr'
 
 echo 'Updating Firebase rules...'
-curl -X PUT --data-binary @firebase_rules.json 'https://raccoon-c86bb.firebaseio.com/.settings/rules.json?auth=vTmi70GaTA2uIqbZ2bFWvpJvMGqoJvdhdcQMUxSr'
+curl -X PUT --data-binary @database.rules.json 'https://raccoon-c86bb.firebaseio.com/.settings/rules.json?auth=vTmi70GaTA2uIqbZ2bFWvpJvMGqoJvdhdcQMUxSr'
