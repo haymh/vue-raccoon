ae0cca89a4a810d9dc8ed43024103fef380ca7d3
heng meng <heng.meng@mobivity.com>
2017-09-06 17:06:32 -0700

    adds new icons for clusters

--- src/components/map/mapSettings.js
+++ src/components/map/mapSettings.js
@@ -12,8 +12,10 @@ const settings = new Vue({
 // https://github.com/xkjyeah/vue-google-maps/issues/38#issuecomment-276677389
 loaded.then(() => {
   /* google.maps.Size is now available! */
-  settings.defaultIconSettings = { url: './static/small_house.png' };
-  settings.defaultClusterIconSettings = { url: './static/m1.png' };
+  settings.house = { url: './static/small_house.png' };
+  settings.cluster3 = { url: './static/c3.png', anchor: { x: 32, y: 32 } };
+  settings.cluster2 = { url: './static/c2.png', anchor: { x: 24, y: 24 } };
+  settings.cluster1 = { url: './static/c1.png', anchor: { x: 18, y: 18 } };
 });
 
 export default settings;
